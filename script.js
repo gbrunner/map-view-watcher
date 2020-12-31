2c016f23c300f74ce321b8d0d8cb36f05278b4fc
Gregory Brunner <gbrunner@esri.com>
2020-12-31 08:59:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,23 @@
-require([
-        "esri/Map",
-        "esri/views/MapView",
-        "esri/layers/Layer"
-      ], function (Map, MapView, Layer) {
+require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
         var map = new Map({
-          basemap: "gray-vector"
+          basemap: "topo-vector"
         });
 
         var view = new MapView({
-          map: map,
           container: "viewDiv",
-          zoom: 7,
-          center: [-90, 40]
+          map: map,
+          zoom: 4,
+          center: [15, 65] // longitude, latitude
         });
 
-        Layer.fromPortalItem({
-          portalItem: {
-            // autocasts as new PortalItem()
-            /*id: "c0f09a71805f4d45a6b6f3f1e0bdc7fc"*/
-            /*id: "af1ad38816814b7eba3fe74a3b84412d"*/
-            id: "c0238b8ae811443dbe52437496a1a514"
-          }
-        })
-          .then(addLayer)
-          .catch(rejection);
 
-        // Adds the layer to the map once it loads
-        function addLayer(layer) {
-          map.add(layer);
-        }
+view.watch("center, scale", (value, oldValue, propertyName)=>{
+  if (propertyName === "center"){
+    console.log(value.x, value.y);
+  } else {
+    console.log("New Value: "+value);
+    console.log("Old Value: "+oldValue);
+  }
+});
 
-        function rejection(error) {
-          console.log("Layer failed to load: ", error);
-        }
-      });
+});
